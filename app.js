59a0678d0bd154db9143315c833e29a60c5b1a34
SHAIKAYUB <142675719+ShaikAyub7@users.noreply.github.com>
2023-10-25 16:32:21 +0530

    roll

--- app.js
+++ app.js
@@ -4,7 +4,9 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl   = document.querySelector('.dice');
-
+const btnRoll  = document.querySelector('.btn--roll');
+const btnNew   = document.querySelector('.btn--new');
+const btnHold  = document.querySelector('.btn--hold');
 //starting condition
 score0El.textContent = 0;
 score1El.textContent = 0;
