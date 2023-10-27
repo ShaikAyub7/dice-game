f645a6a7dbb3eb3455b5af01df8c5ebf2b339fa1
SHAIKAYUB <142675719+ShaikAyub7@users.noreply.github.com>
2023-10-27 20:54:11 +0530

    dice

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-'use strict';
+"use strict";
 
-//selecting elements
-const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
-const diceEl   = document.querySelector('.dice');
-const btnRoll  = document.querySelector('.btn--roll');
-const btnNew   = document.querySelector('.btn--new');
-const btnHold  = document.querySelector('.btn--hold');
+//selecting elements//
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const diceEl   = document.querySelector(".dice");
+const btnRoll  = document.querySelector(".btn--roll");
+const btnNew   = document.querySelector(".btn--new");
+const btnHold  = document.querySelector(".btn--hold");
 //starting condition
 score0El.textContent = 0;
 score1El.textContent = 0;
-diceEl.classList.add('hidden');
+diceEl.classList.add("hidden");
 
-//rolling dice
+//rolling dice functionality//
 
-btnRoll.addEventListener('click', function(){
+  btnRoll.addEventListener("click", function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
 
-const dice = Math.trunc(Math.random() * 6) + 1;
-console.log(dice);
-
-diceEl.classList.remove('hidden');
-diceEl.src = `dice-${dice}.png`;
-
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${dice}.png`;
 });
