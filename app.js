eb6bae86909658ff5cdeb74aa76d669e92b01425
SHAIKAYUB <142675719+ShaikAyub7@users.noreply.github.com>
2023-10-28 14:37:54 +0530

    dice

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 //selecting elements//
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
+const currentEl0 = document.getElementById('current--0');
+const currentEl1 = document.getElementById('current--1');
 const diceEl   = document.querySelector(".dice");
 const btnRoll  = document.querySelector(".btn--roll");
 const btnNew   = document.querySelector(".btn--new");
@@ -11,6 +13,8 @@ const btnHold  = document.querySelector(".btn--hold");
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
+let currentScore = 0;
+let activePlayer = 0;
 
 //rolling dice functionality//
 
@@ -20,4 +24,14 @@ diceEl.classList.add("hidden");
 
   diceEl.classList.remove("hidden");
   diceEl.src = `dice-${dice}.png`;
+
+
+  if(dice != 1)  {
+    currentScore = currentScore + dice ;
+    currentEl0.textContent = currentScore;
+  } else {
+    activePlayer =  currentEl1 + dice;
+    currentEl1.textContent = currentScore;
+
+  }
 });
