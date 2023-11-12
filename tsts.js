a18fe67e4fe5ef1ee6a27ecac2676f5b9d608d29
SHAIKAYUB <142675719+ShaikAyub7@users.noreply.github.com>
2023-11-12 15:28:48 +0530

    .

--- tsts.js
+++ tsts.js
@@ -2,25 +2,25 @@ const hi = "ayub" ;
 
 
 if (hi == "ayub") {
-  console.log(" Hi ayub how are you ");
+  // console.log(" Hi ayub how are you ");
 }else {
 
-   console.log('you are not a ayub  can i now your name');
+  //  console.log('you are not a ayub  can i now your name');
 };
 
 let date = 18 ;
 
 if (date = 17) {
- console.log("today is 16TH november");
+//  console.log("today is 16TH november");
 }else {
-    console.log("today is not 16TH november");
+    // console.log("today is not 16TH november");
 };
 
 
 const ayub = function (){
 ('hi you logged the console with function');
 };
-console.log(ayub);
+// console.log(ayub);
 // console.log(ayub);
 
 //  let naam = "ayuu";
@@ -29,3 +29,8 @@ console.log(ayub);
 // } else {
 //     console.log("noooooooooooooooooooooooooooo");
 // }
+
+// console.log(typeof ayub);
+let yub =  (3+ "3" ) * 3;
+
+console.log( yub)
