74895d7849c6b5f15e4c9d843505c3388c3d97ec
SHAIKAYUB <142675719+ShaikAyub7@users.noreply.github.com>
2023-11-18 14:39:22 +0530

    .

--- Test.js
+++ Test.js
@@ -1,11 +1,17 @@
-console.log("in this");
-let firstName = "saad";
-console.log(name);
+// console.log("in this");
+// let firstName = "saad";
+// console.log(name);
 
-const ayub = function () {};
+// const ayub = function () {};
 
 
-const arsalan = [1990, 1991, 2002, 2001];
-console.log(arsalan[1]);
+// const arsalan = [1990, 1991, 2002, 2001];
+// console.log(arsalan[0]);
 
 // console.log(saad);
+
+
+const arr = [7, 2, 2];
+// const badNewArr = [1, 2, 4];
+
+console.log(arr)
